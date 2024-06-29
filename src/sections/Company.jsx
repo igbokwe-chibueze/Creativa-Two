7da6f99a3d4017a37ba1fd7e9dc3f773e040e93d
igbokwe-chibueze <igbokwechibueze@gmail.com>
2024-06-29 04:45:51 +0100

    Company Section Created.
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- src/sections/Company.jsx
+++ src/sections/Company.jsx
@@ -5,11 +5,30 @@ const Company = () => {
         <div className=" max-w-screen-xl mx-auto text-center px-4 lg:px-12 py-8 lg:py-16 ">
 
             {/* Header */}
-            <h1 
-                className=" mb-4 text-4xl md:text-5xl lg:text-6xl font-extrabold tracking-tight leading-none text-skin-base "
-            >
-                Company
+            <h1 className=" header-one ">
+              Home to the software teams
             </h1>
+
+            {/* SubHearder */}
+            <p className=" paragraph-two ">
+              Meet your developers where they already are. GitHub is home to over 40 million developers and the world’s largest open source community.
+            </p>
+
+            <div className=" sm:px-16 xl:px-48 grid grid-cols-2 md:grid-cols-3">
+              <div>
+                <h2 className=" header-one ">73M+</h2>
+                <p className=" paragraph-one ">developers</p>
+              </div>
+              <div>
+                <h2 className=" header-one ">1B+</h2>
+                <p className=" paragraph-one ">contributors</p>
+              </div>
+              <div>
+                <h2 className=" header-one ">4M+</h2>
+                <p className=" paragraph-one ">organizations</p>
+              </div>
+
+            </div>
         </div>
     </section>
   )
